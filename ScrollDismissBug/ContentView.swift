c2730dd8093b54c1dd0a2c7336accefaa64a887b
Kyosuke Takayama <loiseau@gmail.com>
2023-07-11 16:16:49 +0900

    initial import

---
+++ ScrollDismissBug/ContentView.swift
@@ -0,0 +1,11 @@
+//
+//  ContentView.swift
+//  ScrollDismissBug
+//
+//  Created by Kyosuke Takayama on 2023/07/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+}
---
+++ ScrollDismissBug/ScrollDismissBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollDismissBugApp.swift
+//  ScrollDismissBug
+//
+//  Created by Kyosuke Takayama on 2023/07/11.
+//
+
+import SwiftUI
+
+@main
+struct ScrollDismissBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
